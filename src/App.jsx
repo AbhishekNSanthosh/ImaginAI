e1dca386752bd2f75cf10f6e9ce2bf77c961397f
Abhishek <you@example.com>
2023-09-28 08:32:48 +0530

    updates2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './Modules/Home/Home'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home/>
+      <Home />
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
     </>
   )
 }
